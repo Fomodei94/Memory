9e1eb71f637b017eca0339c8a01ff1fb555f38ee
Federico <federico.omodei@gmail.com>
2020-04-21 01:09:01 +0200

    now featuring difficulty level and card-set choice, fixed play_btn issue when pressed multiple times in a row

--- memory.js
+++ memory.js
@@ -1,90 +1,173 @@
 function startGame() {
-   
+    const start_btn = document.querySelector('#startgame_btn');
     const diffSelector = document.querySelector('#difficulty');
     const cardsetSelector = document.querySelector('#cardset');
+
+    start_btn.innerHTML = 'Reload';
+    start_btn.setAttribute('onclick', 'location.reload()');
     var diffLevel = Number(diffSelector.options[diffSelector.selectedIndex].value);
     var cardset = Number(cardsetSelector.options[cardsetSelector.selectedIndex].value);
-    
+    var cardArray = [];
+    var cbackPath;
+
     if(cardset === 1) {
-        document.addEventListener('DOMContentLoaded', () => {
-    
-            var cbackPath = 'images/Avengers/cardbackAvg01.png';
-    
-            const cardArray = [
-                {
-                    name: 'thor',
-                    img: 'images/Avengers/thor.png'
-                },
-                {
-                    name: 'black_widow',
-                    img: 'images/Avengers/blackw.png'
-                },
-                {
-                    name: 'hulk',
-                    img: 'images/Avengers/hulk.png'
-                },
-                {
-                    name: 'iron_man',
-                    img: 'images/Avengers/ironm.png'
-                },
-                {
-                    name: 'thor',
-                    img: 'images/Avengers/thor.png'
-                },
-                {
-                    name: 'black_widow',
-                    img: 'images/Avengers/blackw.png'
-                },
-                {
-                    name: 'hulk',
-                    img: 'images/Avengers/hulk.png'
-                },
-                {
-                    name: 'iron_man',
-                    img: 'images/Avengers/ironm.png'
-                }
-            ]
-    
-            if(diffLevel === 2 || diffLevel === 3) {
-                cardArray.push({
-                    name: 'loki',
-                    img: 'images/Avengers/loki.png'
-                },
-                {
-                    name: 'loki',
-                    img: 'images/Avengers/loki.png'
-                },
-                {
-                    name: 'hawkeye',
-                    img: 'images/Avengers/hawke.png'
-                },
-                {
-                    name: 'hawkeye',
-                    img: 'images/Avengers/hawke.png'
-                }
-                ) }
-            
-            if(diffLevel === 3) {
-                cardArray.push(
-                {
-                    name: 'nick_fury',
-                    img: 'images/Avengers/nickf.png'
-                },
-                {
-                    name: 'nick_fury',
-                    img: 'images/Avengers/nickf.png'
-                },
-                {
-                    name: 'capt_america',
-                    img: 'images/Avengers/cap.png'
-                },
-                {
-                    name: 'capt_america',
-                    img: 'images/Avengers/cap.png'
-                }
-                )}
-    
+
+        cbackPath = 'images/Avengers/cardbackAvg01.png';
+
+        cardArray = [
+            {
+                name: 'thor',
+                img: 'images/Avengers/thor.png'
+            },
+            {
+                name: 'black_widow',
+                img: 'images/Avengers/blackw.png'
+            },
+            {
+                name: 'hulk',
+                img: 'images/Avengers/hulk.png'
+            },
+            {
+                name: 'iron_man',
+                img: 'images/Avengers/ironm.png'
+            },
+            {
+                name: 'thor',
+                img: 'images/Avengers/thor.png'
+            },
+            {
+                name: 'black_widow',
+                img: 'images/Avengers/blackw.png'
+            },
+            {
+                name: 'hulk',
+                img: 'images/Avengers/hulk.png'
+            },
+            {
+                name: 'iron_man',
+                img: 'images/Avengers/ironm.png'
+            }
+        ]
+
+        if(diffLevel === 2 || diffLevel === 3) {
+            cardArray.push({
+                name: 'loki',
+                img: 'images/Avengers/loki.png'
+            },
+            {
+                name: 'loki',
+                img: 'images/Avengers/loki.png'
+            },
+            {
+                name: 'hawkeye',
+                img: 'images/Avengers/hawke.png'
+            },
+            {
+                name: 'hawkeye',
+                img: 'images/Avengers/hawke.png'
+            }
+            ) }
         
+        if(diffLevel === 3) {
+            cardArray.push(
+            {
+                name: 'nick_fury',
+                img: 'images/Avengers/nickf.png'
+            },
+            {
+                name: 'nick_fury',
+                img: 'images/Avengers/nickf.png'
+            },
+            {
+                name: 'capt_america',
+                img: 'images/Avengers/cap.png'
+            },
+            {
+                name: 'capt_america',
+                img: 'images/Avengers/cap.png'
+            }
+            )}
+    }
+
+    if(cardset === 2) {
+
+        cbackPath = 'images/Videogames/cardbackV03.png';
+
+        cardArray = [
+            {
+                name: 'ezio',
+                img: 'images/Videogames/ezio.png'
+            },
+            {
+                name: 'bordelands',
+                img: 'images/Videogames/borderlands.png'
+            },
+            {
+                name: 'kratos',
+                img: 'images/Videogames/kratos.png'
+            },
+            {
+                name: 'witcher',
+                img: 'images/Videogames/witcher.png'
+            },
+            {
+                name: 'ezio',
+                img: 'images/Videogames/ezio.png'
+            },
+            {
+                name: 'bordelands',
+                img: 'images/Videogames/borderlands.png'
+            },
+            {
+                name: 'kratos',
+                img: 'images/Videogames/kratos.png'
+            },
+            {
+                name: 'witcher',
+                img: 'images/Videogames/witcher.png'
+            }
+        ]
+
+        if(diffLevel === 2 || diffLevel === 3) {
+            cardArray.push({
+                name: 'diablo',
+                img: 'images/Videogames/diablo.png'
+            },
+            {
+                name: 'dragonage',
+                img: 'images/Videogames/dragonage.png'
+            },
+            {
+                name: 'diablo',
+                img: 'images/Videogames/diablo.png'
+            },
+            {
+                name: 'dragonage',
+                img: 'images/Videogames/dragonage.png'
+            }
+            ) }
+        
+        if(diffLevel === 3) {
+            cardArray.push(
+            {
+                name: 'warcraft',
+                img: 'images/Videogames/warcraft.png'
+            },
+            {
+                name: 'oblivion',
+                img: 'images/Videogames/oblivion.png'
+            },
+            {
+                name: 'warcraft',
+                img: 'images/Videogames/warcraft.png'
+            },
+            {
+                name: 'oblivion',
+                img: 'images/Videogames/oblivion.png'
+            }
+            )}
+    }
+
         cardArray.sort(() => 0.5 - Math.random());
 
         const grid = document.querySelector('.grid')
@@ -123,17 +206,24 @@ function startGame() {
             }
             attemptsCounter++;
             displayAttempts.textContent = attemptsCounter;
-            if (attemptsCounter >= 11) {
-                document.getElementById("moves").style.color = 'red';
+            switch(diffLevel) {
+                case 1:
+                    if(attemptsCounter>=7) document.getElementById("moves").style.color = 'red';
+                    break;
+                case 2:
+                    if(attemptsCounter>=11) document.getElementById("moves").style.color = 'red';
+                    break;
+                case 3:
+                    if(attemptsCounter>=14) document.getElementById("moves").style.color = 'red';
+                    break;
             }
+
             cardsChosen = [];
             cardsChosenId = [];
             resultDisplay.textContent = cardsWon.length;
             if (cardsWon.length === cardArray.length/2) {
                 document.getElementById("victory").style.display = 'inline';
                 document.getElementById("play_btn").style.display = 'block';
-                // setTimeout(location.reload, 10000);
-                // alert('Congrats, you found them all!');
             }
         }
     
@@ -149,9 +239,4 @@ function startGame() {
         }
     
         createBoard();
-    
-    }) 
-    
-    }
-
 }
